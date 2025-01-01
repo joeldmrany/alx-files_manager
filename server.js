d6c5549fceeeabc29e7e95a6b74f651a1d92e4a5
joeldmrany <joeldmrany1@gmail.com>
2025-01-01 18:08:58 +0200

    try

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.all('./routes/indexed.js', (req, res, next) => {
-  console.log('Accessing all routes');
-  next();
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to my API');
 });
 
 app.listen(port, () => {
